1a265cfcb29f7143514767c2f4ac623e3828ff6f
anku-07 <adityamondal48765@gmail.com>
2024-03-13 13:54:39 +0530

    Add all file

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 const express = require('express');
+
 const app = express();
-
-app.use(function(req,res,next){
-  console.log("I am middleware");
-  next();
+// app.set('view engine','ejs');
+app.set('view engine','ejs');
+app.get('/home/:user',function(req,res){
+  res.render('index');
 })
 
-app.use(function(req,res,next){
-  console.log("I am middleware 2");
-  next();
-})
-app.get('/',function(req,res){
-  res.send('Hello i am your queries');
-})
 
-app.get('/profile',function(req,res){
-  res.send('Hello i am from your profile');
-})
-
-app.get('/contact',function(req,res){
-  res.send('Hello i am contact route');
-})
-app.get('/payment',function(req,res){
-  res.send('Hello i am from payment');
-})
-
-app.listen(3005);
+app.listen(8080);
