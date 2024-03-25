e1f9458ea0c993f55eb179e5f340f15efec6b07d
anku-07 <adityamondal48765@gmail.com>
2024-03-25 08:07:35 +0530

    Add all files

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const app = express();
-// app.set('view engine','ejs');
 app.set('view engine','ejs');
-app.get('/home/:user',function(req,res){
+app.use(express.static('./public'))
+app.get('/home',function(req,res){
   res.render('index');
 })
+
+app.get('/contact',function(req,res){
+  res.render('contact');
+})
+
 
 
 app.listen(8080);
